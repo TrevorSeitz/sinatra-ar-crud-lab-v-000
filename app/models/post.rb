2383643e97829e8e4a11f215d3a1d9bf78b1584a
Trevor Seitz <trevinla@gmail.com>
2018-05-15 14:46:27 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,26 +2,26 @@
 
 class Post < ActiveRecord::Base
 
-  attr_accessor :name, :content
-
-  # POSTS = []
-  @@all = []
-
-  def initialize(params)
-    @name = params[:name]
-    @content = params[:content]
-    # POSTS << self
-    @@all << self
-  end
-
-  def self.all
-    # POSTS
-    @@all
-  end
-
-  def self.clear
-    # POSTS.clear
-    @@all.clear
-  end
+  # attr_accessor :name, :content
+  #
+  # # POSTS = []
+  # @@all = []
+  #
+  # def initialize(params)
+  #   @name = params[:name]
+  #   @content = params[:content]
+  #   # POSTS << self
+  #   @@all << self
+  # end
+  #
+  # def self.all
+  #   # POSTS
+  #   @@all
+  # end
+  #
+  # def self.clear
+  #   # POSTS.clear
+  #   @@all.clear
+  # end
 
 end
