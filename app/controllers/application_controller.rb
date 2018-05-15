9782804fff220da501ceeaafd1fe8157a32f6cb4
Trevor Seitz <trevinla@gmail.com>
2018-05-15 13:55:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id' do
-    @post = Post.find_by(:id params[:id])
+    @post = Post.find_by(id: params[:id])
 
     erb :show
   end
